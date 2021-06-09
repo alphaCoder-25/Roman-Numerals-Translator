1648abc0e7ef6faf5cdd74231d4f9813e40554c0
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-06-09 22:13:13 +0530

    wire fetch call done

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const inputText = document.querySelector("#txt-input");
 
 const outputDiv = document.querySelector("#output");
 
+const serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
 
 function clickEventHandler() {
-    outputDiv.innerText = "conversion is - " + inputText.value;
+   const txtInput = inputText.value;
+  
+  fetch(getTranslationURL(txtInput))
+  .then(response => response.json())
+  .then(json => {
+     const translatedText = json.contents.translated;
+     outputDiv.innerText = translatedText;
+  })
+   
 };
 
-btnTranslate.addEventListener("click", clickEventHandler);
-
+btnTranslate.addEventListener("click", clickEventHandler)
